fe789b514d704bbf134f2efc19dbd6228f31a023
Pawel Strzelewicz <@>
2021-09-06 16:09:57 +0200

    Add unit tests validate_raw_data: marking, lighting, verification status

--- obstacle_faa_dof_db/test/test_dof_raw_data_validator.py
+++ obstacle_faa_dof_db/test/test_dof_raw_data_validator.py
@@ -44,7 +44,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
             "amsl": "00657",
             "vert_acc_code": "H",
             "hor_acc_code": 8,
-            "loghting_code": "N",
+            "lighting_code": "N",
             "marking_code": "S",
             "verif_status_code": "O"
         }
@@ -54,7 +54,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
             "amsl": 657,
             "vert_acc_code": "H",
             "hor_acc_code": 8,
-            "loghting_code": "N",
+            "lighting_code": "N",
             "marking_code": "S",
             "verif_status_code": "O"
         }
@@ -71,7 +71,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
             "amsl": "00657a",
             "vert_acc_code": "H",
             "hor_acc_code": 8,
-            "loghting_code": "N",
+            "lighting_code": "N",
             "marking_code": "S",
             "verif_status_code": "O"
         }
@@ -81,7 +81,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
             "amsl": None,
             "vert_acc_code": "H",
             "hor_acc_code": 8,
-            "loghting_code": "N",
+            "lighting_code": "N",
             "marking_code": "S",
             "verif_status_code": "O"
         }
@@ -98,7 +98,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
             "amsl": "00657",
             "vert_acc_code": "P",
             "hor_acc_code": 8,
-            "loghting_code": "N",
+            "lighting_code": "N",
             "marking_code": "S",
             "verif_status_code": "O"
         }
@@ -108,7 +108,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
             "amsl": 657,
             "vert_acc_code": None,
             "hor_acc_code": 8,
-            "loghting_code": "N",
+            "lighting_code": "N",
             "marking_code": "S",
             "verif_status_code": "O"
         }
@@ -118,28 +118,106 @@ class DOFRawDataValidatorTest(unittest.TestCase):
         self.assertEqual("Attribute vert_acc_code value error. Unknown vertical accuracy code. Actual value: P ",
                          validator._err_msg)
 
-        def test_validate_raw_data_hor_acc_is_none(self):
-            raw_data = {
-                "agl": "00093",
-                "amsl": "00657",
-                "vert_acc_code": "B",
-                "hor_acc_code": "A",
-                "loghting_code": "N",
-                "marking_code": "S",
-                "verif_status_code": "O"
-            }
+    def test_validate_raw_data_hor_acc_is_none(self):
+        raw_data = {
+            "agl": "00093",
+            "amsl": "00657",
+            "vert_acc_code": "B",
+            "hor_acc_code": 10,
+            "lighting_code": "N",
+            "marking_code": "S",
+            "verif_status_code": "O"
+        }
 
-            converted_data = {
-                "agl": 93.0,
-                "amsl": 657,
-                "vert_acc_code": "B",
-                "hor_acc_code": None,
-                "loghting_code": "N",
-                "marking_code": "S",
-                "verif_status_code": "O"
-            }
+        converted_data = {
+            "agl": 93.0,
+            "amsl": 657,
+            "vert_acc_code": "B",
+            "hor_acc_code": None,
+            "lighting_code": "N",
+            "marking_code": "S",
+            "verif_status_code": "O"
+        }
 
-            validator = DOFRawDataValidator()
-            self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
-            self.assertEqual("Attribute hor_acc_code value error. Unknown horizontal accuracy code. Actual value: B ",
-                             validator._err_msg)
+        validator = DOFRawDataValidator()
+        self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
+        self.assertEqual("Attribute hor_acc_code value error. Unknown horizontal accuracy code. Actual value: 10 ",
+                            validator._err_msg)
+
+    def test_validate_raw_lighting_code_is_none(self):
+        raw_data = {
+            "agl": "00093",
+            "amsl": "00657",
+            "vert_acc_code": "B",
+            "hor_acc_code": 1,
+            "lighting_code": "A",
+            "marking_code": "S",
+            "verif_status_code": "O"
+        }
+
+        converted_data = {
+            "agl": 93.0,
+            "amsl": 657,
+            "vert_acc_code": "B",
+            "hor_acc_code": 1,
+            "lighting_code": None,
+            "marking_code": "S",
+            "verif_status_code": "O"
+        }
+
+        validator = DOFRawDataValidator()
+        self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
+        self.assertEqual("Attribute lighting_code value error. Unknown lighting code. Actual value: A ",
+                            validator._err_msg)
+
+    def test_validate_raw_marking_code_is_none(self):
+        raw_data = {
+            "agl": "00093",
+            "amsl": "00657",
+            "vert_acc_code": "B",
+            "hor_acc_code": 1,
+            "lighting_code": "N",
+            "marking_code": "A",
+            "verif_status_code": "O"
+        }
+
+        converted_data = {
+            "agl": 93.0,
+            "amsl": 657,
+            "vert_acc_code": "B",
+            "hor_acc_code": 1,
+            "lighting_code": "N",
+            "marking_code": None,
+            "verif_status_code": "O"
+        }
+
+        validator = DOFRawDataValidator()
+        self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
+        self.assertEqual("Attribute marking_code value error. Unknown marking code. Actual value: A ",
+                            validator._err_msg)
+
+    def test_validate_verif_status_code_is_none(self):
+        raw_data = {
+            "agl": "00093",
+            "amsl": "00657",
+            "vert_acc_code": "B",
+            "hor_acc_code": 1,
+            "lighting_code": "N",
+            "marking_code": "P",
+            "verif_status_code": "A"
+        }
+
+        converted_data = {
+            "agl": 93.0,
+            "amsl": 657,
+            "vert_acc_code": "B",
+            "hor_acc_code": 1,
+            "lighting_code": "N",
+            "marking_code": "P",
+            "verif_status_code": None
+        }
+
+        validator = DOFRawDataValidator()
+        self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
+        self.assertEqual("Attribute verif_status_code value error. Unknown verification status. Actual value: A ",
+                            validator._err_msg)
