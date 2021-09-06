d32794b72237d59b6bbffdd0e862003f2ffb3371
Pawel Strzelewicz <@>
2021-09-06 16:13:27 +0200

    Fixes, refactor: dof raw data validation module

--- obstacle_faa_dof_db/dof_raw_data_validator.py
+++ obstacle_faa_dof_db/dof_raw_data_validator.py
@@ -1,13 +1,6 @@
+""" Raw data from Digital Obstacle File validator/converter. """
 from ObstacleDatabase_FAA_DOF.obstacle_faa_dof_db.dof_conversion_errors import *
 
-
-_lighting_codes = [
-    "R", "D", "H", "S", "F", "C", "W", "L", "N", "U"
-]
-
-_marking_codes = [
-    "P", "W", "M", "F", "S", "N", "U"
-]
 
 _vert_acc_codes = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I"
@@ -15,6 +8,14 @@ _vert_acc_codes = [
 
 _hor_acc_codes = [
     1, 2, 3, 4, 5, 6, 7, 8, 9
+]
+
+_lighting_codes = [
+    "R", "D", "H", "M", "S", "F", "C", "W", "L", "N", "U"
+]
+
+_marking_codes = [
+    "P", "W", "M", "F", "S", "N", "U"
 ]
 
 _verif_status = [
@@ -26,31 +27,17 @@ class DOFRawDataValidator:
 
     def __init__(self):
         self._err_msg = ""
-        self._parsed_data = {
-            "agl": None,
-            "amsl": None,
-            "vert_acc_code": None,
-            "hor_acc_code": None,
-            "quantity": None,
-            "loghting_code": None,
-            "marking_code": None,
-            "verif_status_code": None
-        }
-
         self._obst_convert_functions = {
             "agl": DOFRawDataValidator.float_value_optional,
             "amsl": DOFRawDataValidator.positive_integer_required,
             "vert_acc_code": DOFRawDataValidator.vert_acc_code_validation,
             "hor_acc_code": DOFRawDataValidator.hor_acc_code_validation,
-            "quantity": None,
-            "loghting_code": DOFRawDataValidator.lighting_code_validation,
+            "lighting_code": DOFRawDataValidator.lighting_code_validation,
             "marking_code": DOFRawDataValidator.marking_code_validation,
             "verif_status_code": DOFRawDataValidator.verif_status_validation
         }
 
-    def _reset_parsed_data(self):
-        for key in self._obstacle_parsed:
-            self._obstacle_parsed[key] = None
+        self.attributes_to_check = self._obst_convert_functions.keys()
 
     @staticmethod
     def float_value_optional(value):
@@ -88,7 +75,7 @@ class DOFRawDataValidator:
             raise UnknownMarkingCode(value)
 
     @staticmethod
-    def vart_acc_code_validation(value):
+    def vert_acc_code_validation(value):
         if value in _vert_acc_codes:
             return value
         else:
@@ -96,26 +83,31 @@ class DOFRawDataValidator:
 
     @staticmethod
     def hor_acc_code_validation(value):
-        if value in _hor_acc_codes:
+        if int(value) in _hor_acc_codes:
             return value
         else:
             raise UnknownHorAccCode(value)
 
     @staticmethod
     def verif_status_validation(value):
-        if value in _vert_acc_codes:
+        if value in _verif_status:
             return value
         else:
             raise UnknownVerifStatus(value)
 
-    def convert_raw_data(self, raw_data):
-        self._reset_parsed_data()
+    def validate_raw_data(self, raw_data):
+        self._err_msg = ""
+        parsed_data = {}
 
         for attribute, value in raw_data.items():
-            try:
-                self._obstacle_parsed[attribute] = self._obst_convert_functions[attribute](value)
-            except Exception as e:
-                if self._err_msg:
-                    self._err_msg += "| Attribute {} value error. {} ".format(attribute, e)
-                else:
-                    self._err_msg += "Attribute {} value error. {} ".format(attribute, e)
+            if attribute in self.attributes_to_check:
+                try:
+                    parsed_data[attribute] = self._obst_convert_functions[attribute](value)
+                except Exception as e:
+                    parsed_data[attribute] = None
+                    if self._err_msg:
+                        self._err_msg += "| Attribute {} value error. {} ".format(attribute, e)
+                    else:
+                        self._err_msg += "Attribute {} value error. {} ".format(attribute, e)
+
+        return parsed_data
