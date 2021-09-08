447821db2f664a00f41e62675c55f524d178dccc
Pawel Strzelewicz <@>
2021-09-08 23:41:11 +0200

    Fix: test_dof_raw_data_validator module typos, style, import statement

--- obstacle_faa_dof_db/test/test_dof_raw_data_validator.py
+++ obstacle_faa_dof_db/test/test_dof_raw_data_validator.py
@@ -1,5 +1,6 @@
 import unittest
-from ObstacleDatabase_FAA_DOF.obstacle_faa_dof_db.dof_raw_data_validator import *
+from obstacle_faa_dof_db.dof_utils.dof_raw_data_validator import *
+from obstacle_faa_dof_db.dof_utils.dof_conversion_errors import *
 
 
 class DOFRawDataValidatorTest(unittest.TestCase):
@@ -16,7 +17,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
     def test_positive_integer_required_convert_to_positive_integer(self):
         self.assertEqual(23, DOFRawDataValidator.positive_integer_required("0023"))
 
-    def test_positive_integer_required_PositiveIntegerNumberRequired_raised_when_not_poistive_integer(self):
+    def test_positive_integer_required_PositiveIntegerNumberRequired_raised_when_not_positive_integer(self):
         self.assertRaises(PositiveIntegerNumberRequired, DOFRawDataValidator.positive_integer_required, "-23")
 
     def test_positive_integer_required_PositiveIntegerNumberRequired_raised_when_not_integer(self):
@@ -25,13 +26,13 @@ class DOFRawDataValidatorTest(unittest.TestCase):
     def test_positive_integer_required_PositiveIntegerNumberRequired_raised_when_empty_value(self):
         self.assertRaises(PositiveIntegerNumberRequired, DOFRawDataValidator.positive_integer_required, "")
 
-    def test_lighting_code_validation_ligthing_code_returned_when_valid(self):
+    def test_lighting_code_validation_lighting_code_returned_when_valid(self):
         self.assertEqual("R", DOFRawDataValidator.lighting_code_validation("R"))
 
     def test_lighting_code_validation_UnknownLightingCode_raised_when_not_valid_code(self):
         self.assertRaises(UnknownLightingCode, DOFRawDataValidator.lighting_code_validation, "Z")
 
-    def test_marking_code_validation_ligthing_code_returned_when_valid(self):
+    def test_marking_code_validation_lighting_code_returned_when_valid(self):
         self.assertEqual("F", DOFRawDataValidator.marking_code_validation("F"))
 
     def test_marking_code_validation_UnknownLightingCode_raised_when_not_valid_code(self):
@@ -142,7 +143,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
         validator = DOFRawDataValidator()
         self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
         self.assertEqual("Attribute hor_acc_code value error. Unknown horizontal accuracy code. Actual value: 10 ",
-                            validator._err_msg)
+                         validator._err_msg)
 
     def test_validate_raw_lighting_code_is_none(self):
         raw_data = {
@@ -168,7 +169,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
         validator = DOFRawDataValidator()
         self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
         self.assertEqual("Attribute lighting_code value error. Unknown lighting code. Actual value: A ",
-                            validator._err_msg)
+                         validator._err_msg)
 
     def test_validate_raw_marking_code_is_none(self):
         raw_data = {
@@ -194,7 +195,7 @@ class DOFRawDataValidatorTest(unittest.TestCase):
         validator = DOFRawDataValidator()
         self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
         self.assertEqual("Attribute marking_code value error. Unknown marking code. Actual value: A ",
-                            validator._err_msg)
+                         validator._err_msg)
 
     def test_validate_verif_status_code_is_none(self):
         raw_data = {
@@ -220,4 +221,4 @@ class DOFRawDataValidatorTest(unittest.TestCase):
         validator = DOFRawDataValidator()
         self.assertEqual(converted_data, validator.validate_raw_data(raw_data))
         self.assertEqual("Attribute verif_status_code value error. Unknown verification status. Actual value: A ",
-                            validator._err_msg)
+                         validator._err_msg)
