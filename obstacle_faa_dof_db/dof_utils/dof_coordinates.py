2322117e1a7508b52a1cbdbc173be2ceb90b76f6
Pawel Strzelewicz <@>
2021-09-08 07:03:49 +0200

    Add coordinates conversion (dmsh to dd)

---
+++ obstacle_faa_dof_db/dof_utils/dof_coordinates.py
@@ -0,0 +1,31 @@
+def check_hemisphere(hem, coord_type):
+    """
+    param: hem: string, hemisphere character
+    param: coord_type: string, coordinate type: LATITUDE or LONGITUDE
+    return: bool
+    """
+    if coord_type == "LATITUDE":
+        if hem in ['N', 'S']:
+            return True
+    elif coord_type == "LONGITUDE":
+        if hem in ['E', 'W']:
+            return True
+
+
+def dmsh_to_dd(src_coord, coord_type):
+    """ Converts DMS format of longitude, latitude from DOF data into DD format.
+    param: src_coord: string, latitude or longitude in degrees, minutes, seconds format
+    return: dd: float, latitude or longitude in decimal degrees format
+    """
+    h = src_coord[-1]
+    if check_hemisphere(h, coord_type):
+        coord_parts = src_coord.split(' ')
+        d = float(coord_parts[0])
+        m = float(coord_parts[1])
+        s = float(coord_parts[2][:-1])
+
+        dd = d + m / 60 + s / 3600
+
+        if h in ['W', 'S']:
+            dd = - dd
+        return dd
